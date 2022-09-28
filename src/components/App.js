e8615b67a2a4dc252bd485d4229d289381edd3bd
Marcus Glaze <marcusglz94@gmail.com>
2022-09-27 23:40:00 -0400

    18. Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import '../App.css';
+import config from '../config.json';
 import { 
   loadProvider, 
   loadNetwork,
@@ -9,7 +10,7 @@ import {
   loadTokens,
   loadExchange } from '../store/interactions';
 
-const config = require('../config.json');
+import Navbar from './Navbar';
 
 function App() {
 
@@ -22,23 +23,25 @@ function App() {
     // fetch current network's chainID
     const { chainId } = await loadNetwork(provider, dispatch);
 
+    // reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload();
+    })
+
     // fetch current account & balance from Metamask
-    await loadAccount(provider, dispatch);
+    window.ethereum.on('accountsChanged', () => {
+      loadAccount(provider, dispatch);
+    })
+    
 
     // load token smart contracts
     const dApp = config[chainId].dApp;
     const mEth = config[chainId].mEth;
-    await loadTokens(
-      provider, 
-      [dApp.address, mEth.address], 
-      dispatch);
+    await loadTokens(provider, [dApp.address, mEth.address], dispatch);
 
     // load exchange smart contract
     const exchange = config[chainId].exchange;
-    await loadExchange(
-      provider,
-      exchange.address,
-      dispatch);
+    await loadExchange(provider, exchange.address, dispatch);
   }
 
   useEffect(() => {
@@ -49,6 +52,7 @@ function App() {
     <div>
 
       {/* Navbar */}
+      <Navbar/>
 
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
